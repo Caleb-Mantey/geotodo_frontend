75a631362b72e2d9d41e24bd69e8743c07b0410b
Caleb Mantey <manteycaleb@gmail.com>
2021-12-29 21:02:16 +0000

    frontend code

--- src/component/add-todo.js
+++ src/component/add-todo.js
@@ -1,47 +1,50 @@
-import React, { Component } from 'react';
-import {InputGroup,InputGroupAddon,Input,Button} from 'reactstrap';
+import React, { Component } from "react";
+import { InputGroup, InputGroupAddon, Input, Button } from "reactstrap";
 import Axios from "axios";
 
 class AddTodo extends Component {
-    constructor(){
-        super();
-        this.handleText = this.handleText.bind(this);
-        this.handleClick = this.handleClick.bind(this);
-    }
+  constructor() {
+    super();
+    this.handleText = this.handleText.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-    state = { 
-        title: "",
-        error: ""
-     }
+  state = {
+    title: "",
+    error: "",
+  };
 
-    handleText (e){
+  handleText(e) {
+    this.setState({
+      title: e.target.value,
+    });
+  }
+
+  handleClick() {
+    Axios.post("http://localhost:5500/api/todos/", { title: this.state.title })
+      .then((res) => {
+        console.log(res);
         this.setState({
-            title: e.target.value
-        })
-    }
+          title: "",
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-    handleClick(){
-        Axios.post('http://localhost:5000/api/todos/', {title: this.state.title})
-        .then((res) => {
-            console.log(res);
-            this.setState({
-                title: ""
-            })
-        }).catch((err) => {
-            console.log(err);
-        })
-    }
-
-    render() { 
-        return (
-        <div>
-            <InputGroup>
-                <Input onChange={this.handleText}  value={this.state.title}/>
-                <InputGroupAddon addonType="prepend"><Button onClick={this.handleClick}>Add a Todo</Button></InputGroupAddon>
-            </InputGroup>
-        </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <InputGroup>
+          <Input onChange={this.handleText} value={this.state.title} />
+          <InputGroupAddon addonType="prepend">
+            <Button onClick={this.handleClick}>Add a Todo</Button>
+          </InputGroupAddon>
+        </InputGroup>
+      </div>
+    );
+  }
 }
- 
+
 export default AddTodo;
--- src/component/todo-list.js
+++ src/component/todo-list.js
@@ -1,5 +1,5 @@
 import Axios from "axios";
-import React, {Component} from "react";
+import React, { Component } from "react";
 import {
   ListGroup,
   ListGroupItem,
@@ -10,80 +10,90 @@ import {
   Card,
   InputGroup,
   InputGroupAddon,
-  Input
+  Input,
 } from "reactstrap";
 import TodoListItem from "./todo-list-item";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 class TodoList extends Component {
-    constructor(){
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    state = {  
-        todos: []
-    }
+  state = {
+    todos: [],
+  };
 
-    getTodos(){
-        Axios.get('http://localhost:5000/api/todos/')
-        .then((res) => {
-            this.setState({
-                todos: res.data
-            })
-            console.log(res);
-            console.log(this.state.todos);
-        }).catch((err) => {
-            console.log(err);
-        })
-    }
+  getTodos() {
+    Axios.get("http://localhost:5500/api/todos/")
+      .then((res) => {
+        this.setState({
+          todos: res.data,
+        });
+        console.log(res);
+        console.log(this.state.todos);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-    deleteTodo(id){
-        Axios.delete(`http://localhost:5000/api/todos/${id}`)
-        .then((res) => {
-            console.log(res);
-        }).catch((err) => {
-            console.log(err);
-        })
-    }
+  deleteTodo(id) {
+    Axios.delete(`http://localhost:5500/api/todos/${id}`)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-    udpdateTodo(id,title){
-        Axios.put('http://localhost:5000/api/todos/', {title: title, id: id})
-        .then((res) => {
-            console.log(res);
-            this.notify();
-        }).catch((err) => {
-            console.log(err);
-        })
-    }
+  udpdateTodo(id, title) {
+    Axios.put("http://localhost:5500/api/todos/", { title: title, id: id })
+      .then((res) => {
+        console.log(res);
+        this.notify();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-    notify() {
-        toast("Todo Updated Successfully!");
-    }
+  notify() {
+    toast("Todo Updated Successfully!");
+  }
 
-    componentDidMount(){
-        this.getTodos();
-    }
+  componentDidMount() {
+    this.getTodos();
+  }
 
-    componentWillUpdate() {
-        this.getTodos();
-    }
+  componentWillUpdate() {
+    this.getTodos();
+  }
 
-    render() { 
-        return (
-        <div>
-             <ToastContainer />
-            <h1>Todo List Item</h1>
-            <ListGroup>
-                {this.state.todos.length > 0 ? this.state.todos.map(item => {
-                    return <TodoListItem key={item.id} item = {item} onDelete={this.deleteTodo} onUpdate={this.udpdateTodo}/>
-                })  : null}
-            
-            </ListGroup>
-           
-        </div>
-          );
-    }
+  render() {
+    return (
+      <div>
+        <ToastContainer />
+        <h1>Todo List Item</h1>
+        <ListGroup>
+          {this.state.todos.length > 0
+            ? this.state.todos.map((item) => {
+                return (
+                  <TodoListItem
+                    key={item.id}
+                    item={item}
+                    onDelete={this.deleteTodo}
+                    onUpdate={this.udpdateTodo}
+                  />
+                );
+              })
+            : null}
+        </ListGroup>
+      </div>
+    );
+  }
 }
 
 export default TodoList;
